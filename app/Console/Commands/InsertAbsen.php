d2a45948a1d8726b958b9172c83211c5e3e11303
Ibra <farhanibra21@gmail.com>
2024-06-14 08:38:14 +0700

    Add Check Data to Commands

--- app/Console/Commands/InsertAbsen.php
+++ app/Console/Commands/InsertAbsen.php
@@ -29,22 +29,30 @@ class InsertAbsen extends Command
     public function handle()
     {
         $siswa = Siswa::all();
+        $tanggal = Carbon::now()->toDateString();
 
-        foreach ($siswa as $item){
-            $absen = new Absen();
-            $absen->id_siswa = $item->id;
-            $absen->status = 'Tidak Hadir';
-            $absen->jam_masuk = null;
-            $absen->foto_masuk = null;
-            $absen->lokasi_masuk = null;
-            $absen->jam_keluar = null;
-            $absen->foto_keluar = null;
-            $absen->lokasi_keluar = null;
-            $absen->foto = null;
-            $absen->keterangan = null;
-            $absen->keterangan = null;
-            $absen->tanggal = Carbon::now()->toDateString();
-            $absen->save();
+
+
+        foreach ($siswa as $item) {
+            $cek = Absen::where('id_siswa', $item->id)->whereDate('tanggal', $tanggal)->first();
+
+            if (!$cek) {
+
+                $absen = new Absen();
+                $absen->id_siswa = $item->id;
+                $absen->status = 'Tidak Hadir';
+                $absen->jam_masuk = null;
+                $absen->foto_masuk = null;
+                $absen->lokasi_masuk = null;
+                $absen->jam_keluar = null;
+                $absen->foto_keluar = null;
+                $absen->lokasi_keluar = null;
+                $absen->foto = null;
+                $absen->keterangan = null;
+                $absen->keterangan = null;
+                $absen->tanggal = Carbon::now()->toDateString();
+                $absen->save();
+            }
         }
 
         $this->info('Data Harian telah Di Masukkan');
