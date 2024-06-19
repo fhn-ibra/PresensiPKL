0766834aa4cf55409c243d120b08817d90059cc5
Aldo-Aja <revaldoparikesit@gmail.com>
2024-06-19 18:33:17 +0700

    Fix: Button Option

--- resources/views/Siswa/opsi.blade.php
+++ resources/views/Siswa/opsi.blade.php
@@ -17,40 +17,39 @@
         <div class="col">
             <div class="row" style="margin-top:70px">
                 <div class="col-12">
-                    <div class="card mb-1" style="border : 1px solid blue">
-                        <div class="card-body">
-                            <div class="historicontent">
-                                <div class="iconpresensi">
-                                    <ion-icon name="accessibility-outline" style="font-size: 48px;"
-                                    class="text-success"></ion-icon>
-                                </div>
-                                <div class="datapresensi">
-                                    <h3 style="line-height: 3px">HADIR</h3>
-                                    <span>Jam Masuk : 07.00</span><br>
-                                    <span>Jam Pulang : 16.00</span>
-                                </div>
-                            </div>
+                  <div class="card mb-1 clickable-card" style="border : 1px solid blue">
+                    <a href="#">  <div class="card-body">
+                        <div class="historicontent">
+                          <div class="iconpresensi">
+                            <ion-icon name="accessibility-outline" style="font-size: 48px;" class="text-success"></ion-icon>
+                          </div>
+                          <div class="datapresensi">
+                            <h3 style="line-height: 3px">HADIR</h3>
+                            <span>Jam Masuk : 07.00</span><br>
+                            <span>Jam Pulang : 16.00</span>
+                          </div>
                         </div>
-                    </div>
+                      </div>
+                    </a>  </div>
                 </div>
                 <div class="col-12">
-                    <div class="card mb-1" style="border : 1px solid blue">
-                        <div class="card-body">
-                            <div class="historicontent">
-                                <div class="iconpresensi">
-                                    <ion-icon name="ban-outline" style="font-size: 48px;"
-                                    class="text-danger"></ion-icon>
-                                </div>
-                                <div class="datapresensi">
-                                    <h3 style="line-height: 3px">TIDAK HADIR</h3>
-                                    <span>Jam Masuk : 07.00</span><br>
-                                    <span>Jam Pulang : 16.00</span>
-                                </div>
-                            </div>
+                  <div class="card mb-1 clickable-card" style="border : 1px solid blue">
+                    <a href="#">  <div class="card-body">
+                        <div class="historicontent">
+                          <div class="iconpresensi">
+                            <ion-icon name="ban-outline" style="font-size: 48px;" class="text-danger"></ion-icon>
+                          </div>
+                          <div class="datapresensi">
+                            <h3 style="line-height: 3px">TIDAK HADIR</h3>
+                            <span>Lampirkan Surat Izin</span><br>
+                            <span>Serta Keterangan</span>
+                          </div>
                         </div>
-                    </div>
+                      </div>
+                    </a>  </div>
                 </div>
-            </div>
+              </div>
+              
         </div>
     </div>
 @endsection
